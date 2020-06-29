8b5641afccc64ee66d9d42fa2b4d442e41377ded
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-29 18:38:59 +0530

    feat: Add code for problem 1

--- Problem_1.py
+++ Problem_1.py
@@ -10,3 +10,59 @@ For this first problem I designed a data structure known as Least Recently Used 
 
 All operations must take O(1) time.
 '''
+
+from collections import OrderedDict
+
+
+class LRU_Cache(object):
+    def __init__(self, capacity):
+        # Initialize class variables
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent.
+
+        try:
+            get_v = self.cache.pop(key)
+            self.cache[key] = get_v
+            return get_v
+
+        except KeyError:
+            return -1
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+
+        if self.capacity == 0:
+            print("Can't perform operations on 0 capacity cache")
+            return
+
+        if key in self.cache:  # Update priority due to access
+            self.cache.pop(key)
+            self.cache[key] = value
+
+        else:  # Add to cache
+            if len(self.cache) < self.capacity:  # Still space on cache
+                self.cache[key] = value
+
+            else:  # No space available on cache
+                self.cache.popitem(last=False)
+                self.cache[key] = value
+
+
+# Test Case
+
+our_cache = LRU_Cache(5)
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
+print(our_cache.get(1))  # returns 1
+print(our_cache.get(2))  # returns 2
+print(our_cache.get(9))  # returns -1
+our_cache.set(5, 5)
+our_cache.set(6, 6)
+print(our_cache.get(3))  # returns -1 because key 3 was thrown out
+our_cache.set(7, 7)
+print(our_cache.get(4))  # 4 is thrown out
