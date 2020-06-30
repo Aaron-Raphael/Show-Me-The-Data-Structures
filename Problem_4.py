0b62bbfa7539be8d51fea9301387c4b5a2b9616e
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-06-30 20:54:06 +0530

    feat: Add problem 4 question

---
+++ Problem_4.py
@@ -0,0 +1,6 @@
+'''
+4. Active Directory
+
+In Windows Active Directory, a group can consist of user(s) and group(s) themselves. We can construct this hierarchy as such. Where User is represented by str representing their ids.
+In this problem I have written a function that provides an efficient look up of wether the use is in a group.
+'''
