60fd979f7eba966cb9dc4ec6d3301ed41a287396
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-07-01 21:27:00 +0530

    feat: Add question for problem 6

---
+++ Problem_6.py
@@ -0,0 +1,7 @@
+'''
+## 6. Union and Intersection of Two Linked Lists
+
+In this problem I had to write the union and intersection functions. The union of two sets A and B is the set of elements which are in A, in B, or in both A and B. The intersection of two sets A and B, denoted by A ∩ B, is the set of all objects that are members of both the sets A and B.
+
+I had taken in the the two linked lists and return a linked list that is composed of either the union or intersection respectively.
+'''
