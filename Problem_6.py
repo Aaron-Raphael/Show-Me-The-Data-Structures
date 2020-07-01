403aef98ee3e6a80dd8a28ea48ab290151a4891f
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-07-01 21:27:34 +0530

    Add code for problem 5

--- Problem_6.py
+++ Problem_6.py
@@ -5,3 +5,123 @@ In this problem I had to write the union and intersection functions. The union o
 
 I had taken in the the two linked lists and return a linked list that is composed of either the union or intersection respectively.
 '''
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+        return out_string
+
+
+    def append(self, value):
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def size(self):
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+
+        return size
+
+    def to_list(self):
+
+        return_list = []
+        node = self.head
+
+        while node:
+            return_list.append(node.value)
+            node = node.next
+
+        return return_list
+
+def union(llist_1, llist_2):
+    
+    list_1 = llist_1.to_list()
+    list_2 = llist_2.to_list()
+
+    union_set = set(list_1 + list_2)
+
+    return_list = LinkedList()
+
+    for _ in union_set:
+        return_list.append(_)
+
+    return return_list
+
+def intersection(llist_1, llist_2):
+    
+    list_1 = llist_1.to_list()
+    list_2 = llist_2.to_list()
+
+    intersection_set = set()
+    for element in list_1:
+        if element in list_2:
+            intersection_set.add(element)
+
+    return_list = LinkedList()
+
+    for _ in intersection_set:
+        return_list.append(_)
+
+    return return_list
+
+
+# Test case 1
+
+linked_list_1 = LinkedList()
+linked_list_2 = LinkedList()
+
+element_1 = [3,2,4,35,6,65,6,4,3,21]
+element_2 = [6,32,4,9,6,1,11,21,1]
+
+for i in element_1:
+    linked_list_1.append(i)
+
+for i in element_2:
+    linked_list_2.append(i)
+
+print (union(linked_list_1,linked_list_2))
+print (intersection(linked_list_1,linked_list_2))
+
+# Test case 2
+
+linked_list_3 = LinkedList()
+linked_list_4 = LinkedList()
+
+element_1 = [3,2,4,35,6,65,6,4,3,23]
+element_2 = [1,7,8,9,11,21,1]
+
+for i in element_1:
+    linked_list_3.append(i)
+
+for i in element_2:
+    linked_list_4.append(i)
+
+print (union(linked_list_3,linked_list_4))
+print (intersection(linked_list_3,linked_list_4))
