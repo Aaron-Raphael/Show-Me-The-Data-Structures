8f3ec9006bcc97dced630256a48fa22e32027cfd
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-07-01 21:22:58 +0530

    feat: Add question for problem 5

---
+++ Problem_5.py
@@ -0,0 +1,10 @@
+'''
+5. Blockchain
+
+A Blockchain is a sequential chain of records, similar to a linked list. Each block contains some information and how it is connected related to the other blocks in the chain. Each block contains a cryptographic hash of the previous block, a timestamp, and transaction data. For our blockchain we will be using a SHA-256 hash, the Greenwich Mean Time when the block was created and text strings as the data.
+
+We can breakdown the blockchain down into having 3 main parts.
+- First is the information hash
+- The next main component is the block on the blockchain
+- Finally link all of this together in a block chain
+'''
